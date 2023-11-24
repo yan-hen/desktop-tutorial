7b1b109ead71989a53675d3f45ef9e53fb138e8b
yan-hen <151193497+yan-hen@users.noreply.github.com>
2023-11-24 10:51:27 +0800

    1

---
+++ 1.py
@@ -0,0 +1,42 @@
+class Python:
+    def _inlt_ (self,course_name,course_id,course_credits):
+        self.course_name=course_name
+        self.ourse_id=course_id
+        self.course_credits=""
+
+class Student:
+    def _inlt_ (self,stu_name,stu_id):
+        self.stu_name=stu_name
+        self.stu_id= stu_id
+
+def _str_(self):
+ return f"Name:{self.name}\nID ={self.Course_id}"
+
+def addCourse(self,object):
+    self.course =object
+
+c1= Course(course_name ="套裝軟體應用",course_id="G0D17M01",course_credits=3)
+
+s1=Student=(stu_name ="梁雅祺",stu_id="1B1G0031")
+s1.addCourse(ourse_info=c1)
+
+
+
+
+
+
+
+
+
+def _inlt_ (self,name,age):
+        self.name=name
+        self.age=age
+        
+        
+        
+p1=Python ("John,36")
+print(p1)
+
+p2=Python("Sue",22)
+print(p2._dict_)
+\nCourse={self.Course_info.get}\
